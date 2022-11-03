57161883f2f66f255aef6c51287c0af9e47ea901
jiongyunsun <js11116@nyu.edu>
2022-11-03 01:20:56 -0400

    initial seed selection

--- timer/script.js
+++ timer/script.js
@@ -79,9 +79,11 @@ class Timer {
               <span class="timer__part timer__part--minutes">00</span>
               <span class="timer__part">:</span>
               <span class="timer__part timer__part--seconds">00</span>
+              
               <button type="button" class="timer__btn timer__btn--control timer__btn--start">
                   <span class="material-icons">play_arrow</span>
               </button>
+
               <button type="button" class="timer__btn timer__btn--reset">
                   <span class="material-icons">timer</span>
               </button>
