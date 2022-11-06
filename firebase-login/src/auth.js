5e529146f6771f584836f76525335ca2709a2ba9
Harry Liu <harryliu23333@gmail.com>
2022-11-06 00:54:00 -0400

    Firebase Login Setup

---
+++ firebase-login/src/auth.js
@@ -0,0 +1,6 @@
+export const loginEmail = document.getElementById("logEmail");
+export const loginPassword = document.getElementById("logPassword");
+export const regEmail = document.getElementById("regEmail");
+export const regPassword = document.getElementById("regPassword");
+export const loginBtn = document.getElementById("loginBtn");
+export const regBtn = document.getElementById("regBtn");
