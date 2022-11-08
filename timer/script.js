9db3748c880168964d19e275350e7501592bd60a
jiongyunsun <js11116@nyu.edu>
2022-11-07 20:20:54 -0500

    fix countdown in mins

--- timer/script.js
+++ timer/script.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> remotes/origin/Rita
 class Timer {
     constructor(root) {
       root.innerHTML = Timer.getHTML();
@@ -48,6 +52,11 @@ class Timer {
         this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
         this.el.control.classList.add("timer__btn--stop");
         this.el.control.classList.remove("timer__btn--start");
+<<<<<<< HEAD
+        
+
+=======
+>>>>>>> remotes/origin/Rita
       }
     }
   
@@ -79,9 +88,11 @@ class Timer {
               <span class="timer__part timer__part--minutes">00</span>
               <span class="timer__part">:</span>
               <span class="timer__part timer__part--seconds">00</span>
+              
               <button type="button" class="timer__btn timer__btn--control timer__btn--start">
                   <span class="material-icons">play_arrow</span>
               </button>
+
               <button type="button" class="timer__btn timer__btn--reset">
                   <span class="material-icons">timer</span>
               </button>
