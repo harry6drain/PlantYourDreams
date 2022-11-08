4be1caaf6f8a137df5da2c9888afe40697cb5275
Harry Liu <harryliu23333@gmail.com>
2022-11-07 23:03:30 -0500

    Updated Firestore Auth & Firestore DB

--- firebase-login/src/auth.js
+++
@@ -1,6 +0,0 @@
-export const loginEmail = document.getElementById("logEmail");
-export const loginPassword = document.getElementById("logPassword");
-export const regEmail = document.getElementById("regEmail");
-export const regPassword = document.getElementById("regPassword");
-export const loginBtn = document.getElementById("loginBtn");
-export const regBtn = document.getElementById("regBtn");
