e2d2b5a588645131df40d9da96a3c4dae900421a
Harry Liu <harryliu23333@gmail.com>
2022-11-14 17:09:03 -0500

    Updated Page Redirect

--- firebase-login/public/register.js
+++ firebase-login/public/register.js
@@ -3,23 +3,24 @@ import { auth,db} from "./firebase.js";
 import { collection,addDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 
 
-registerbtn.addEventListener('click',(e) => {
+registerBtn.addEventListener('click',(e) => {
     var email = document.getElementById('email').value;
     var password = document.getElementById('psw').value;
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(async (userCredential) => {
         // Signed in
         const user = userCredential.user;
         var lgDate = new Date();
         // ... user.uid
         alert(`${user.uid} created successfully!`);
         try {
-            const docRef = addDoc(collection(db, "users"), {
+            const docRef = await addDoc(collection(db, "users"), {
               uid: user.uid,
               email: user.email,
               lastLogin: lgDate
             });
             console.log("Document written with ID: ", docRef.id);
+            window.location.assign("./index.html")
           } catch (error) {
             console.error("Error adding document: ", error);
         }
