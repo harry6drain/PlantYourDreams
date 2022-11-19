c51cca5a14f8d9dc6633522c48788cbbb9deb0fe
jiongyunsun <js11116@nyu.edu>
2022-11-19 14:58:02 -0500

    fix the active navigation link

--- src/Script.js
+++ src/Script.js
@@ -20,6 +20,15 @@ let btnstart=document.getElementById("start");
 let btnanother=document.getElementById("another");
 let grown=document.getElementById("grown");
 let selection;
+let links = document.querySelectorAll('.nav-link');
+
+for(let i=0; i<links.length; i++){
+  links[i].addEventListener('click', function() {
+    for(let j=0; j<links.length; j++)
+      links[j].classList.remove('active');
+    this.classList.add('active');
+  });
+}
 
 btnSelect.disabled=true;
 // import{promptMe} from "./timerscript.js"
