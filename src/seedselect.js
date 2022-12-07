08feb53e9211fdb659bd9ae70d07488b7c9cfd98
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2022-12-07 17:41:35 -0500

    fix bug of db read on Mainpage.html

--- src/seedselect.js
+++ src/seedselect.js
@@ -2,6 +2,7 @@ import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import{ getFirestore} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import {getAuth,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { db, app } from './firebase.js'
 
 let dand_select=document.getElementById("dandelion");
 let three_select=document.getElementById("threeleaf");
@@ -10,24 +11,13 @@ let cotton_select=document.getElementById("cotton");
 let msg=document.getElementById("msg")
 let User;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAUS-fQ46Gv4kuLEqvYurwQJ5inCRkLA1U",
-  authDomain: "plantyourdreams-8d08e.firebaseapp.com",
-  databaseURL: "https://plantyourdreams-8d08e-default-rtdb.firebaseio.com",
-  projectId: "plantyourdreams-8d08e",
-  storageBucket: "plantyourdreams-8d08e.appspot.com",
-  messagingSenderId: "63192119407",
-  appId: "1:63192119407:web:c15c2b763e51e7f786b59a",
-  measurementId: "G-XYRQCEW5L5"
-};
-
-const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const db=getFirestore(app);
 const auth = getAuth(app);
 
 const uid=window.sessionStorage.getItem("uid") //get user id from current session
-const docRef = doc(db,"seed",uid);//get user seed collection info
+
+
+const docRef = doc(db,"seed",uid);//get u ser seed collection info
 const docSnap = await getDoc(docRef);
 // let seeds=docSnap.data().Inventory;
 
